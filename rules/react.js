65d2502944583ead01fafcffa130118725ead6a2
a.khmyrov <a.khmyrov@bestdoctor.ru>
2022-10-05 11:43:49 +0300

    fix: fix react/function-component-definition settings

--- rules/react.js
+++ rules/react.js
@@ -7,7 +7,10 @@ module.exports = {
     'react/no-unused-prop-types': 'off',
     'react/require-default-props': 'off',
     'react/jsx-no-bind': ['off', { allowFunctions: true }],
-    'react/function-component-definition': [2, { "namedComponents": "arrow-function" }],
+    'react/function-component-definition': [2, {
+      "namedComponents": "arrow-function",
+      "unnamedComponents": "arrow-function",
+    }],
   },
 
   settings: {
